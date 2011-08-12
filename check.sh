6ef4b6868136f6ffcf5b65a19ccd665679d720fa
ashejn <ashejn@yandex-team.ru>
2011-08-12 14:32:29 +0400

    added eblob-version

--- check.sh
+++ check.sh
@@ -9,3 +9,13 @@ if [ "$LAST_VERSION" != "$CURRENT_VERSION" ]; then
     git commit elliptics-version -m "update version to $LAST_VERSION"
     git push
 fi
+
+LAST_VERSION=`find /repo/yandex-lucid/ -name "eblob_*.changes" -exec basename {} \; | awk -F_ '{ print $1"_"$2 }' | sort -uVr | head -n1`
+CURRENT_VERSION=`cat /home/ashejn/elliptics-tree/eblob-version`
+
+if [ "$LAST_VERSION" != "$CURRENT_VERSION" ]; then
+    cd /home/ashejn/elliptics-tree
+    echo $LAST_VERSION > eblob-version
+    git commit eblob-version -m "update version to $LAST_VERSION"
+    git push
+fi
