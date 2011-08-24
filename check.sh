963fd4c4d4347aa35cc35fa1187537721ba1fe9d
Atkady L. Shane <ashejn@yandex-team.ru>
2011-08-24 18:19:40 +0400

    added elliptics-fastcgi-version

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-LAST_VERSION=`find /repo/yandex-lucid/ -name "elliptics_*.changes" -exec basename {} \; | awk -F_ '{ print $1"_"$2 }' | sort -uVr | head -n1`
+LAST_VERSION=`find /repo/yandex-lucid/ -name "elliptics-2.10_*.changes" -exec basename {} \; | awk -F_ '{ print $1"_"$2 }' | sort -uVr | head -n1`
 CURRENT_VERSION=`cat /home/ashejn/elliptics-tree/elliptics-version`
 
 if [ "$LAST_VERSION" != "$CURRENT_VERSION" ]; then
@@ -17,5 +17,15 @@ if [ "$LAST_VERSION" != "$CURRENT_VERSION" ]; then
     cd /home/ashejn/elliptics-tree
     echo $LAST_VERSION > eblob-version
     git commit eblob-version -m "update version to $LAST_VERSION"
+    git push
+fi
+
+LAST_VERSION=`find /repo/yandex-lucid/ -name "elliptics-fastcgi_*.changes" -exec basename {} \; | awk -F_ '{ print $1"_"$2 }' | sort -uVr | head -n1`
+CURRENT_VERSION=`cat /home/ashejn/elliptics-tree/elliptics-fastcgi-version`
+
+if [ "$LAST_VERSION" != "$CURRENT_VERSION" ]; then
+    cd /home/ashejn/elliptics-tree
+    echo $LAST_VERSION > elliptics-fastcgi-version
+    git commit elliptics-fastcgi-version -m "update version to $LAST_VERSION"
     git push
 fi
